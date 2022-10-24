36b3e88ad9ed0edd7a30f3af04769aa0f02f6eb8
Manuel <mosquera.manuel2011@gmail.com>
2022-10-24 08:43:31 -0500

    804 datos clasificados

--- tweets_extractor.py
+++ tweets_extractor.py
@@ -3,6 +3,6 @@ from utils.file_utilities import append_tweets_to_csv
 
 if __name__ == "__main__":
   keyword = "(robaron OR asaltaron OR secuestraron OR apuñalaron OR raponearon) lang:es -is:retweet -is:reply (has:media OR has:images OR has:video_link)"
-  start_time = "2022-10-22T00:47:00.000Z"
+  start_time = "2022-10-23T01:43:00.000Z"
   response = make_request(keyword, start_time)
   append_tweets_to_csv(response, 'data.csv')
